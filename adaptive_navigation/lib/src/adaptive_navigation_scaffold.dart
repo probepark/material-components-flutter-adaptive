d84ef666c011907631deeef8a87bdec49b0cf688
probe <re2rar@gmail.com>
2021-09-12 19:43:41 +0900

    Always call tapped event.

--- adaptive_navigation/lib/src/adaptive_navigation_scaffold.dart
+++ adaptive_navigation/lib/src/adaptive_navigation_scaffold.dart
@@ -388,10 +388,7 @@ class AdaptiveNavigationScaffold extends StatelessWidget {
   }
 
   void _destinationTapped(AdaptiveScaffoldDestination destination) {
-    final index = destinations.indexOf(destination);
-    if (index != selectedIndex) {
-      onDestinationSelected?.call(index);
-    }
+    onDestinationSelected?.call(index);
   }
 }
 
